3a0265c495db4de64d851f3fcf7fe583c8db1ed0
kevalunagar <unagarkeval1@gmail.com>
2021-03-08 20:49:21 +0530

    All Labs Uploaded

---
+++ labs/firstdbtest/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+# Create your models here.
+
+
+class Student(models.Model):
+    student_name = models.CharField(max_length=100)
+    student_dob = models.DateTimeField('date published')
--- nvsp/forms/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from .models import Form
-
-# Create your views here.
-def download(request):
-    forms = Form.objects.all()
-    return render(request, 'form.html', {'forms' : forms})
